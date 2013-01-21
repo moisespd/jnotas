359023ac5e172eac105ff35cf45a9609103f4a59
moi <moisespd@gmail.com>
2013-01-21 11:24:06 +0000

    P1. La clase Notas

--- JNotas/src/Main.java
+++ JNotas/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo\n");
+		Nota nota = new Nota();
+		nota.setTitulo("Nota de prueba");
+		nota.setDescripcion("Esto es simplemente una nota de prueba");
+		nota.setPrioridad(100);
+		System.out.println(nota.toString());
+		
 	}
 }
