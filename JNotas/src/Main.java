5a98d6ce984d9bce9117f373a860cdfcb2ef5928
moi <moisespd@gmail.com>
2013-01-22 11:24:17 +0000

    Base de datos: ejemplo simple abrir, select, cerrar
    Ejemplo simple de funcionamiento de base de datos

--- JNotas/src/Main.java
+++ JNotas/src/Main.java
@@ -1,15 +1,24 @@
+import java.sql.*;
+
 public class Main {
 	public static void main(String[] args) {
-		Nota nota;
 		try {
-			FrmNota frame = new FrmNota();
-			frame.setVisible(true);
-			nota = new Nota();
-			nota.get(100);
-			frame.setNota(nota);
+		   Class.forName("com.mysql.jdbc.Driver");
+		   
+		   Connection conn = DriverManager.getConnection ("jdbc:mysql://localhost/jnotasdb","root", "teidetron");
+		   Statement s = conn.createStatement(); 
+		   ResultSet rs = s.executeQuery ("select * from Notas");
+		   while (rs.next()) 
+		   { 
+		       System.out.println (rs.getInt (1) + " " + rs.getString (2)+ " " + rs.getDate(3)); 
+		   }
+		   s.close();
+		   rs.close();
+		   conn.close();
+		   System.out.println("Funciona el driver!!!");
 		} 
 		catch (Exception e) {
-			System.out.println("Error!!!");
+		   e.printStackTrace();
 		}
 	}
 }
